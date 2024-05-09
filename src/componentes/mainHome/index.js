b44a686a251eefabe269b393e73c3b81c9ddcb33
djairb <djair.batista@outlook.com>
2024-05-09 16:50:26 -0300

    slide mudando com o fade

--- src/componentes/mainHome/index.js
+++ src/componentes/mainHome/index.js
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
 
-import { register } from 'swiper/element/bundle'
+import { register } from 'swiper/element/bundle';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
+import 'swiper/css/effect-fade';  // Import the fade effect CSS
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 
+// No need to import EffectCoverflow as fade effect doesn't require it
 
-import { EffectCoverflow } from 'swiper/modules';
-
-import 'lightbox.js-react/dist/index.css'
-import {SlideshowLightbox} from 'lightbox.js-react'
+import 'lightbox.js-react/dist/index.css';
+import { SlideshowLightbox } from 'lightbox.js-react';
 
 import imagem01 from "../../img/mainHome/01.png";
 import imagem02 from "../../img/mainHome/02.png";
@@ -25,83 +23,68 @@ register();
 
 function MainHome() {
 
-    const listaFotoHome = [
+  const listaFotoHome = [
 
-        {id: 1, imagem:imagem01},
-        {id: 2, imagem:imagem02}
+    { id: 1, imagem: imagem01 },
+    { id: 2, imagem: imagem02 },
+
+  ];
 
 
-    ];
+  return (
 
+    <section>
 
-    return (
+      <Swiper
 
-        <section>
+        slidesPerView={1}
 
-            <Swiper
+        style={{
 
-                slidesPerView={1}
+          "--swiper-pagination-color": "#72c000",
+          "--swiper-navigation-color": "#72c000",
 
-                style={{
+        }}
 
-                    "--swiper-pagination-color": "#72c000",
-                    "--swiper-navigation-color": "#72c000",
+        effect={'fade'}  // Set the effect to 'fade'
+        grabCursor={true}
+        loop={true}  // Maintain loop for continuous fading
 
-                }}
+        autoplay={{
+          delay: 2000
+        }}
 
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={2}
-                loop={true}
+        pagination={{ clickable: true }}
+        navigation
+        className="swiperMaster"
 
-                coverflowEffect={
-                    {
-                        rotate: 0,
-                        stretch: 0,
-                        depth: 100,
-                        modifier: 2.5,
-                        slideShadows: true,
+      >
+        {listaFotoHome.map((item) => (
 
-                    }
-                }
-                autoplay={{
-                    delay: 2000
-                }}
-                modules={[EffectCoverflow]}
-                pagination={{ clickable: true }}
-                navigation
-                className="swiperMaster"
-                >
-                {listaFotoHome.map((item) => (
+          <SwiperSlide key={item.id}>
 
-                    <SwiperSlide key={item.id}>
+            <SlideshowLightbox
+              fullScreen={true}
+              showControls={true}
+              modalClose="clickOutside"
+              className="grid grid-cols-3 gap-2 mx-auto"
+            >
 
-                        
+              <img className="w-full rounded standImg" src={item.imagem} />
 
-                        <SlideshowLightbox
-                        ///fullsreen true pra imagens muito grandes-
-                            fullScreen={true}
-                            showControls={true}
-                            modalClose="clickOutside"
-                            className="grid grid-cols-3 gap-2 mx-auto">
+            </SlideshowLightbox>
 
-                            <img className="w-full rounded standImg" src={item.imagem}/>
+          </SwiperSlide>
 
-                        </SlideshowLightbox>
-                        
-                    </SwiperSlide>
-                
-                ))}
-            </Swiper>
+        ))}
+      </Swiper>
 
-            
-
-        </section>
+    </section>
 
 
 
 
-    );
+  );
 }
 
 export default MainHome;
