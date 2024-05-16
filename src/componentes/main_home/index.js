22ad77d2ba006ef208871f7ec57ebd02e1347af7
djairb <djair.batista@outlook.com>
2024-05-16 16:31:31 -0300

    animação ta completo agora

--- src/componentes/main_home/index.js
+++ src/componentes/main_home/index.js
@@ -1,125 +1,84 @@
 import React, { useEffect, useState } from 'react';
-
 import { register } from 'swiper/element/bundle';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
-import 'swiper/css/effect-fade';  // Import the fade effect CSS
+import 'swiper/css/effect-fade';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
-
-// No need to import EffectCoverflow as fade effect doesn't require it
-
 import 'lightbox.js-react/dist/index.css';
 import { SlideshowLightbox } from 'lightbox.js-react';
-
-
-
-
 import { listaFotoHome } from '../../imgBd';
 
 register();
+
 function MainHome() {
     const strings = ["Inovação", "Desenvolvimento", "Cidadania", "Educação", "Inclusão", "Transformação", "Crescimento", "Progresso", "Cuidado", "Colaboração"];
     const [index, setIndex] = useState(0);
     const [text, setText] = useState(strings[0]);
     const [fade, setFade] = useState(false);
-    const textColor = '#FFBF00';
+    const [imageIndex, setImageIndex] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setIndex((prevIndex) => (prevIndex + 1) % strings.length);
-            setFade(true); // Ativar o efeito de fade quando a palavra muda
-        }, 2000); // Tempo de exibição de cada palavra em milissegundos
+            setIndex(prevIndex => (prevIndex + 1) % strings.length);
+            setFade(true);
+        }, 2500);
 
         return () => clearInterval(interval);
     }, []);
 
     useEffect(() => {
         setText(strings[index]);
+        const timeout = setTimeout(() => {
+            setFade(false);
+        }, 2000);
+        return () => clearTimeout(timeout);
     }, [index]);
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            setFade(false); // Desativar o efeito de fade depois de 1 segundo
-        }, 2000); // Tempo de duração do efeito de fade
-
-        return () => clearTimeout(timeout);
-    }, [text]);
+        const interval = setInterval(() => {
+            setImageIndex(prevIndex => (prevIndex + 1) % listaFotoHome.length);
+        }, 3000);
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <section className='sectionTextImg'>
             <div className='mainTexto'>
-
-                <h1 className='mainSomos'>Somos Conexão Social para <span style={{ color: textColor }} className={fade ? 'fade-text fontText' : 'fontText'}>{text}</span></h1>
-                
-            </div>         
-
-        <div className='swiperSlide'>
-
-            <Swiper
-
-                className='swiperReal'
-
-                slidesPerView={1}
-
-                style={{
-
-                "--swiper-pagination-color": "#72c000",
-                "--swiper-navigation-color": "#72c000",
-
-                }}
-
-                effect={'fade'}  // Set the effect to 'fade'
-                grabCursor={true}
-                loop={true}  // Maintain loop for continuous fading
-
-                autoplay={{
-                delay: 2000
-                }}
-
-                pagination={{ clickable: true }}
-                navigation
-                
-
+                <h1 className='mainSomos'>Somos Conexão Social para <br/> <span style={{ color: '#FFBF00' }} className={fade ? 'fade-text fontText' : 'fontText'}>{text}</span></h1>
+            </div>
+            <div className='swiperSlide'>
+                <Swiper
+                    className='swiperReal'
+                    slidesPerView={1}
+                    style={{
+                        "--swiper-pagination-color": "#72c000",
+                        "--swiper-navigation-color": "#72c000",
+                    }}
+                    effect='fade'
+                    grabCursor={true}
+                    loop={true}
+                    autoplay={{ delay: 3000 }}
+                    pagination={{ clickable: true }}
+                    navigation
                 >
-                
-                {listaFotoHome.map((item) => (
-
-                <SwiperSlide key={item.id}>
-
-                    <SlideshowLightbox
-                    fullScreen={true}
-                    showControls={true}
-                    modalClose="clickOutside"
-                    className="swipeImg grid grid-cols-3 gap-2 mx-auto"
-                    >
-
-                        <img className="w-full rounded standImg" src={item.imagem} />
-
-                    </SlideshowLightbox>
-
-                </SwiperSlide>
-
-                ))}
-            </Swiper>
-
-
-
-        </div>       
-
-      
-
-    </section>
-
-    
-
-
-
-
-  );
+                    {listaFotoHome.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            <SlideshowLightbox
+                                fullScreen={true}
+                                showControls={true}
+                                modalClose="clickOutside"
+                                className="swipeImg grid grid-cols-3 gap-2 mx-auto"
+                            >
+                                <img className="w-full rounded standImg" src={item.imagem} />
+                            </SlideshowLightbox>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </section>
+    );
 }
 
 export default MainHome;
