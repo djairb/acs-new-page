05b09d0a1c1c743b316ff212c0d5ebc8ee5476b5
djairb <39774197+djairb@users.noreply.github.com>
2024-07-15 09:37:36 -0300

    Endereço update

--- src/componentes/mensagemVisita/index.js
+++ src/componentes/mensagemVisita/index.js
@@ -9,7 +9,7 @@ function MensagemVisita() {
 
                 <h1 className="visitaH1">Vem visitar a gente!</h1>
 
-                <p className="visitaP">Centro Tecnológico de Inclusão Digital e Social - Rua Projetada A, sem número, Bairro Salinas - Lagoa de Itaenga/PE <br/> Telefone:
+                <p className="visitaP">Centro Tecnológico de Inclusão Digital e Social - R. Rosa Cândido, 35 - Lagoa Do Itaenga, Lagoa do Itaenga - PE, 55840-000 <br/> Telefone/Whatsapp:
                 (081) 97343-2301 <br/> E-mail: <a
                     href="mailto:administrativo@somosconexaosocial.org">administrativo@somosconexaosocial.org</a>
                 </p>
