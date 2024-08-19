9eed3cc4b58dd96ad81b2ec87629547ef4094d5c
djairb <39774197+djairb@users.noreply.github.com>
2024-08-19 16:56:06 -0300

    INSERÇÃO DO LINK PRO SRA

--- src/componentes/nav/index.js
+++ src/componentes/nav/index.js
@@ -29,7 +29,11 @@ function Navbar(){
                 <Link to="/boletins-informativos">Boletins Informativos</Link>
 
                 <Link to="/transparencia">Transparência</Link>
+
                 <Link to="/colaboradores">Colaboradores</Link>
+
+                <a href="https://somosconexaosocial.org/sra" rel="noopener noreferrer">SRA</a>
+
         
         
                 <button className= "nav-btn nav-close-btn" onClick={showNavBar}>
