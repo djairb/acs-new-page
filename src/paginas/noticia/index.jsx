a00a5b0283e840333d2f3a10bf9a5b65f3656e41
djairb <39774197+djairb@users.noreply.github.com>
2025-02-02 16:16:55 -0300

    mais estilizações na pagina de noticias

--- src/paginas/noticia/index.jsx
+++ src/paginas/noticia/index.jsx
@@ -1,44 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import '../../style/styleGpt.css';
+import '../../style/style.css'; // Importe o arquivo CSS
 import { useParams } from 'react-router-dom';
-
-
 import Axios from "axios";
-
 import { SlideshowLightbox } from 'lightbox.js-react';
-import 'lightbox.js-react/dist/index.css'
-
+import 'lightbox.js-react/dist/index.css';
 import { API_BASE_URL_NOTICIAS } from '../../infra/apiConfig';
 import Navbar from '../../componentes/nav';
 import Footer from '../../componentes/folter';
 
-
-
-
 const Noticia = () => {
-
-
     const { slug } = useParams();
-    
-
-   
-
-
     const [loadingGeral, setLoadingGeral] = useState(false);
-
     const [noticiaCarregadaCompleta, setNoticiaCarregadaCompleta] = useState(false);
-
     const [noticiaCarregada, setNoticiaCarregada] = useState(false);
-
     const [loadingImg, setLoadingImg] = useState(false);
-
     const [fotosNoticia, setFotosNoticia] = useState([]);
-
-
-    
-
     const [dataFoto, setDataFoto] = useState(false);
-
 
     const arrayBufferToDataURL = (arrayBuffer, mimeType = 'image/jpeg') => {
         const bytes = new Uint8Array(arrayBuffer);
@@ -50,87 +27,63 @@ const Noticia = () => {
     const formatDate = (isoDate) => {
         const date = new Date(isoDate);
         if (isNaN(date.getTime())) {
-          return ''; // Retorna vazio se a data não for válida
+            return '';
         }
-        // Formatar a data para 'yyyy-MM-dd' (sem a parte de hora)
         const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0'); // Adiciona zero à esquerda se necessário
-        const day = String(date.getDate()).padStart(2, '0'); // Adiciona zero à esquerda se necessário
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
         return `${year}-${month}-${day}`;
     };
-    
-    
-
 
     useEffect(() => {
         const carregarNoticia = async () => {
-          setLoadingGeral(true);
-    
-          try {
-            
-            const responseNoticia = await Axios.get(`${API_BASE_URL_NOTICIAS}/getNoticiaGeralBySlug`, {
-              params: { slug: slug }
-            });
-            setNoticiaCarregada(responseNoticia.data[0]);
-            setNoticiaCarregadaCompleta(true);
-            setDataFoto(arrayBufferToDataURL(new Uint8Array(responseNoticia.data[0].foto_capa.data)))        
-                      
-          } catch (error) {
-            console.error('Erro ao tentar carregar as Notícias:', error);
-            alert("Ocorreu um erro ao tentar carregar notícias. Por favor, tente novamente mais tarde.");
-          } finally {
-            setLoadingGeral(false);
-          }
+            setLoadingGeral(true);
+            try {
+                const responseNoticia = await Axios.get(`${API_BASE_URL_NOTICIAS}/getNoticiaGeralBySlug`, {
+                    params: { slug: slug }
+                });
+                setNoticiaCarregada(responseNoticia.data[0]);
+                setNoticiaCarregadaCompleta(true);
+                setDataFoto(arrayBufferToDataURL(new Uint8Array(responseNoticia.data[0].foto_capa.data)));
+            } catch (error) {
+                console.error('Erro ao tentar carregar as Notícias:', error);
+                alert("Ocorreu um erro ao tentar carregar notícias. Por favor, tente novamente mais tarde.");
+            } finally {
+                setLoadingGeral(false);
+            }
         };
-    
         carregarNoticia();
-      }, [slug]);
+    }, [slug]);
 
-      useEffect(() => {
+    useEffect(() => {
         const carregarFotosNoticia = async () => {
-
             if (!noticiaCarregadaCompleta) return;
-            
-
-            setLoadingImg(true); //mantem o loading aberto
-
+            setLoadingImg(true);
             try {
                 const fotosResponse = await Axios.get(`${API_BASE_URL_NOTICIAS}/getAllFotosByIdNoticiaGeral`, {
-                    
                     params: { id: noticiaCarregada.id_noticia }
                 });
-
                 setFotosNoticia(fotosResponse.data.map((image) => ({
                     ...image,
                     blob: arrayBufferToDataURL(new Uint8Array(image.foto.data))
                 })));
-
-                setLoadingImg(false);               
-                
-
+                setLoadingImg(false);
             } catch (error) {
                 console.error('Erro ao carregar fotos:', error);
                 alert("Ocorreu um erro ao tentar carregar as fotos da turma. Por favor, tente novamente mais tarde.");
                 setLoadingImg(false);
             }
         };
-
         carregarFotosNoticia();
     }, [noticiaCarregadaCompleta]);
 
-
     return (
-
-        <>
-
-        <Navbar />
-
-        <main className='mainNoticia'>
-
-            
-            {loadingGeral && <div className="spinnerButton"><div></div></div>}
-            {noticiaCarregadaCompleta ? ( // Condição para carregar os dados
-                <div>
+        <div className="page-container">
+            <Navbar />
+            <main className="main-noticia">
+                {loadingGeral && <div className="spinner"><div></div></div>}
+                {noticiaCarregadaCompleta ? (
+                    <div className="content-container">
                         <SlideshowLightbox
                             theme="day"
                             fullScreen={false}
@@ -138,52 +91,42 @@ const Noticia = () => {
                             modalClose="clickOutside"
                             className="grid grid-cols-3 gap-2 mx-auto"
                         >
-                            <img src={dataFoto} />
+                            <img src={dataFoto} alt="Capa da Notícia" className="capa-imagem" />
                         </SlideshowLightbox>
 
-                    
+                        <h1 className="titulo-noticia">{noticiaCarregada.titulo}</h1>
+                        <p>{noticiaCarregada.descricao}</p>
 
-                    <h1>{noticiaCarregada.titulo}</h1>
+                        {loadingImg ? <div className="spinner"></div> : (
+                            <div className="carrossel-imagens">
+                                {fotosNoticia.map((image) => (
+                                    <div key={image.id_foto}>
+                                        <SlideshowLightbox
+                                            theme="day"
+                                            fullScreen={false}
+                                            showControls={true}
+                                            modalClose="clickOutside"
+                                            className="grid grid-cols-3 gap-2 mx-auto"
+                                        >
+                                            <img
+                                                src={image.blob}
+                                                alt={`uploaded preview`}
+                                                className="imagem-carrossel"
+                                            />
+                                        </SlideshowLightbox>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
 
-                    <p>{noticiaCarregada.descricao}</p>                   
-
-
-                    {loadingImg ? <div className="spinner"></div> : (fotosNoticia.map((image) => (
-                        <div key={image.id_foto}>
-                            <SlideshowLightbox
-                                theme="day"
-                                fullScreen={false}
-                                showControls={true}
-                                modalClose="clickOutside"
-                                className="grid grid-cols-3 gap-2 mx-auto"
-                            >
-                                <img
-                                    className='w-full rounded'
-                                    src={image.blob}
-                                    alt={`uploaded preview`}
-                                />
-                            </SlideshowLightbox>
-
-                        </div>
-                    )))}
-
-                    <p> Autor: {noticiaCarregada.nome_autor}, em {formatDate(noticiaCarregada.data_noticia)}</p> 
-                
-                </div>
-            ) : (
-                <p>Carregando dados...</p> // Exibição temporária enquanto os dados são carregados
-            )}
-
-
-
-        </main>
-        
-        <Footer />
-
-        </>
-
-
-
+                        <p className="autor-data"> Autor: {noticiaCarregada.nome_autor}, em {formatDate(noticiaCarregada.data_noticia)}</p>
+                    </div>
+                ) : (
+                    <p>Carregando dados...</p>
+                )}
+            </main>
+            <Footer />
+        </div>
     );
 };
 
