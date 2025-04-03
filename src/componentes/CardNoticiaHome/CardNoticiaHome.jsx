0dacbbc9027a37d8fb4d95ddb1fd9a732a081a19
djairb <39774197+djairb@users.noreply.github.com>
2025-04-03 16:00:09 -0300

    correções menores na home

--- src/componentes/CardNoticiaHome/CardNoticiaHome.jsx
+++ src/componentes/CardNoticiaHome/CardNoticiaHome.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import '../../style/style.css';
 import { useNavigate } from 'react-router-dom';
+import { API_IMAGEM_URL } from "../../infra/apiConfig";
 
 export default function CardNoticiaHome(props) {
     const navigate = useNavigate();
 
     const handleClickVisualizar = () => {
         navigate(`/noticias/${props.slug}`);
-    };
-
-    const arrayBufferToDataURL = (arrayBuffer, mimeType = 'image/jpeg') => {
-        const bytes = new Uint8Array(arrayBuffer);
-        const binaryString = Array.from(bytes).map(byte => String.fromCharCode(byte)).join('');
-        const base64String = window.btoa(binaryString);
-        return `data:${mimeType};base64,${base64String}`;
-    };
-
-    const dataFoto = arrayBufferToDataURL(new Uint8Array(props.foto_capa.data));
+    }
 
     return (
         <div className="containerCardNoticiaHome" onClick={handleClickVisualizar}>
             <div className="cardImagemContainer">
                 <img
-                    src={dataFoto}
+                    src={`${API_IMAGEM_URL}${props.foto_capa}`}
                     alt="Capa da Notícia"
                     className="cardImagem"
                 />
